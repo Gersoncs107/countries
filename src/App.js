0bb6bb83fb10b3e633ee74813c32097cece1d5f6
Gersoncs107 <gerson.silva107@gmail.com>
2025-07-10 23:01:57 -0300

    countries-update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import { useState, useEffect } from "react";
-
-
-
+import axios from "axios";
 
 function App() {
 
   const [country, setCountry] = useState([])
 
-  
+ useEffect(() => {
+    axios.get('http://localhost:3001/countries')
+      .then(response => {
+        setCountry(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the countries!", error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <div>
         <label>Find Countries</label>
         <input type='text'/>
+      </div>
+      <div>
+        {country.map((item, index) => (
+          <div key={index}>
+            <h2>{item.name}</h2>
+            <p>Capital: {item.capital}</p>
+            <p>Population: {item.population}</p>
+            <p>Area: {item.area} km²</p>
+          </div>
+        ))}
       </div>
     </div>
   );
