3c7eae1f2389ed9f5626c2dffc84d2f5a443d077
Gersoncs107 <gerson.silva107@gmail.com>
2025-07-17 20:10:27 -0300

    countries-update

--- src/components/ShowCountry.js
+++ src/components/ShowCountry.js
@@ -1,13 +1,15 @@
-const ShowCountry = ({}) => {
+const ShowCountry = ({name, onClick}) => {
     
     return(
         <ul>
               {filteredCountries.map((country) => (
                 <li key={country.cca3 || country.ccn3 || country.cca2}>
-                  {country.name?.common || country.name?.official || "No name"}
-                  <button onClick={() => setSelectedCountry(country)}>Show</button>
+                  {name}
+                  <button onClick={onClick}>Show</button>
                 </li>
               ))}
             </ul>
     )
 }
+
+export default ShowCountry
