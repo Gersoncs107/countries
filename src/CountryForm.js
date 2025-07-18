517d8f616b9a5d3b76b3c92cb2171afe484a7db7
Gersoncs107 <gerson.silva107@gmail.com>
2025-07-17 22:02:09 -0300

    countries-update

---
+++ src/CountryForm.js
@@ -0,0 +1,10 @@
+const CountryForm = ({text, onChange}) => {
+    return (
+        <form>
+            <label>
+           {text} <input onChange={onChange} />
+            </label>
+      </form>
+
+    )
+}
